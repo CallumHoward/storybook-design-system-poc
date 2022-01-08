73c8432e9e65abed23596b340037cd5daf422460
CallumHoward <callum.howard@safetyculture.io>
2022-01-08 20:30:52 +1100

    Improve button story

---
+++ src/components/atoms/Button.stories.tsx
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+import { action } from "@storybook/addon-actions";
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import {
+  Button,
+  BUTTON_SIZES,
+  BUTTON_STATES,
+  BUTTON_TYPES,
+  BUTTON_VARIATIONS,
+} from "./Button";
+
+type Meta = ComponentMeta<typeof Button>;
+type Story = ComponentStoryObj<typeof Button>;
+
+const meta: Meta = {
+  component: Button,
+};
+export default meta;
+
+const LayoutWrapper = styled.div`
+  & > * {
+    margin: 2rem;
+  }
+`;
+
+export const Component: Story = {
+  args: {
+    dataAnchor: "sckit2-button",
+    children: "Button",
+    onClick: action("button-click"),
+  },
+  decorators: [(story) => <LayoutWrapper>{story()}</LayoutWrapper>],
+};
+
+export const Sizes: Story = {
+  ...Component,
+  render: (args) => (
+    <>
+      {BUTTON_SIZES.map((size) => (
+        <Button
+          {...args}
+          dataAnchor={`sckit-button-${size}`}
+          key={size}
+          size={size}
+        />
+      ))}
+    </>
+  ),
+};
+
+export const Types: Story = {
+  ...Component,
+  render: (args) => (
+    <>
+      {BUTTON_TYPES.map((type) => (
+        <Button
+          {...args}
+          dataAnchor={`sckit-button-${type}`}
+          key={type}
+          type={type}
+        />
+      ))}
+    </>
+  ),
+};
+
+export const Variations: Story = {
+  ...Component,
+  render: (args) => (
+    <>
+      {BUTTON_VARIATIONS.map((variation) => (
+        <Button
+          {...args}
+          dataAnchor={`sckit-button-${variation}`}
+          key={variation}
+          variation={variation}
+        />
+      ))}
+    </>
+  ),
+};
+
+export const States: Story = {
+  ...Component,
+  render: (args) => (
+    <>
+      {BUTTON_STATES.map((state) => (
+        <Button
+          {...args}
+          dataAnchor={`sckit-button-${state}`}
+          key={state}
+          state={state}
+        />
+      ))}
+    </>
+  ),
+};
--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,41 +1,55 @@
 import { FunctionComponent } from "react";
+import styled from "styled-components";
+
+export const BUTTON_TYPES = ["primary", "positive", "negative"] as const;
+export const BUTTON_SIZES = ["small", "medium", "large"] as const;
+export const BUTTON_VARIATIONS = ["filled", "outline", "ghost"] as const;
+export const BUTTON_STATES = [
+  "default",
+  "active",
+  "hover",
+  "pressed",
+  "disabled",
+  "loading",
+] as const;
+
+type ButtonTypes = typeof BUTTON_TYPES[number];
+type ButtonSizes = typeof BUTTON_SIZES[number];
+type ButtonVariations = typeof BUTTON_VARIATIONS[number];
+type ButtonStates = typeof BUTTON_STATES[number];
 
 type ButtonProps = {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: "small" | "medium" | "large";
-  /** Button contents */
-  label: string;
+  /** Anchor for targeting with automation */
+  dataAnchor: string; // TODO achieve with forward-ref only?
+  /** Size and spaciousness */
+  size?: ButtonSizes;
+  /** Role type of the button */
+  type?: ButtonTypes;
+  /** Style variations */
+  variation?: ButtonVariations;
+  /** Visual state of the button */
+  state?: ButtonStates;
   /** Optional click handler */
   onClick?: () => void;
 };
+
+const StyledButton = styled.button``;
 
 /**
  * Primary UI component for user interaction
  */
 export const Button: FunctionComponent<ButtonProps> = ({
-  primary = false,
   size = "medium",
-  backgroundColor,
-  label,
+  type = "primary",
+  variation = "filled",
+  state = "default",
+  onClick = () => null,
+  children,
   ...props
 }) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
   return (
-    <button
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <StyledButton type="button" {...props}>
+      {children}
+    </StyledButton>
   );
 };
