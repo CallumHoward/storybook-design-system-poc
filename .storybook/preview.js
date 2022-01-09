db4f038e590865b60bda005104198625d492a311
CallumHoward <callum.howard@safetyculture.io>
2022-01-10 01:46:44 +1100

    Add toolbar items for language and theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -39,3 +39,40 @@ export const parameters = {
     },
   },
 };
+
+export const globalTypes = {
+  locale: {
+    name: "Locale",
+    description: "Internationalization locale",
+    defaultValue: "en",
+    toolbar: {
+      icon: "globe",
+      items: [
+        { value: "en-us", right: "🇺🇸", title: "English US" },
+        { value: "en-gb", right: "🇬🇧", title: "English UK" },
+        { value: "de", right: "🇩🇪", title: "German" },
+        { value: "fr", right: "🇫🇷", title: "French" },
+        { value: "es", right: "🇪🇸", title: "Spanish" },
+        { value: "pt", right: "🇵🇹", title: "Portuguese" },
+      ],
+    },
+  },
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "kakadu-light",
+    toolbar: {
+      icon: "eye",
+      // Array of plain string values or MenuItem shape (see below)
+      items: [
+        "kakadu-light",
+        "kakadu-dark",
+        "canopy-light",
+        "canopy-dark",
+        "legacy",
+      ],
+      // Property that specifies if the name of the item will be displayed
+      // showName: true,
+    },
+  },
+};
